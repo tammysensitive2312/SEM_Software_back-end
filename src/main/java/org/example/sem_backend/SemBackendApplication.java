461a852e4c624557cf236795e6ab96a8f630a713
tammysensitive2312 <dinhtruong1234lhp@gmail.com>
2024-09-30 08:40:18 +0700

    build-structure&dev-tools

---
+++ src/main/java/org/example/sem_backend/SemBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.sem_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sem_backend/SemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sem_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
