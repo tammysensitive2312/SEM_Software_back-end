a121f59aedf628a0cab89e8dbbb1372e6bafe9f1
tammysensitive2312 <dinhtruong1234lhp@gmail.com>
2024-10-15 17:53:01 +0700

    update docs and container-name

--- script.sh
+++ script.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-# Step 1: Khởi động container MySQL từ docker-compose
-echo "Starting MySQL container..."
-docker-compose up -d db
-
-# Step 2: Chờ cho MySQL sẵn sàng
-echo "Waiting for MySQL to be ready..."
-until docker exec -it $(docker-compose ps -q db) mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;" &> /dev/null
-do
-    echo "Waiting for MySQL to be ready..."
-    sleep 1
-done
-
-echo "MySQL is ready and databases are accessible."
-
 # Keep the container running (if necessary)
 tail -f /dev/null
