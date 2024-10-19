0c6651bc6bf7bf362cd2aa3b686114a9ed1ac1e9
tammysensitive2312 <dinhtruong1234lhp@gmail.com>
2024-10-19 22:17:16 +0700

    update api and test curl

--- src/main/java/org/example/sem_backend/main_service/container/repositories/RepositoryContainer.java
+++ src/main/java/org/example/sem_backend/main_service/container/repositories/RepositoryContainer.java
@@ -1,18 +1,10 @@
 package org.example.sem_backend.main_service.container.repositories;
 
 import lombok.RequiredArgsConstructor;
-import org.example.sem_backend.modules.equipment_module.repository.EquipmentDetailRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class RepositoryContainer {
 
-    private final EquipmentDetailRepository equipmentDetailRepository;
-
-    @Bean
-    public EquipmentDetailRepository equipmentDetailRepository() {
-        return equipmentDetailRepository;
-    }
 }
--- src/main/java/org/example/sem_backend/main_service/container/services/ServiceContainer.java
+++ src/main/java/org/example/sem_backend/main_service/container/services/ServiceContainer.java
@@ -1,18 +1,11 @@
 package org.example.sem_backend.main_service.container.services;
 
 import lombok.RequiredArgsConstructor;
-import org.example.sem_backend.modules.equipment_module.service.EquipmentService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class ServiceContainer {
 
-    private final EquipmentService equipmentService;
 
-    @Bean
-    public EquipmentService equipmentService() {
-        return equipmentService;
-    }
 }
