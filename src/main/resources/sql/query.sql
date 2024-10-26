c88bb77ab5773febe947248c0f1e46d715b18080
tammysensitive2312 <dinhtruong1234lhp@gmail.com>
2024-10-26 11:35:23 +0700

    fix conflict part 4

--- src/main/resources/sql/query.sql
+++ src/main/resources/sql/query.sql
@@ -5,18 +5,18 @@ ALTER TABLE equipment DROP CONSTRAINT equipment_chk_1;
 ALTER TABLE equipment
     ADD CONSTRAINT equipment_chk_1 CHECK (category IN ('INFORMATION_TECHNOLOGY_EQUIPMENT', 'LABORATORY_EQUIPMENT', 'SPORTS_EQUIPMENT', 'TEACHING_EQUIPMENT'));
  */
-SELECT e.category, ed.id, ed.code, ed.description, ed.current_status,ed.operating_hours, ed.purchase_date, r.number, r.type
+SELECT e.category, ed.id, ed.code, ed.description, ed.status,ed.operating_hours, ed.purchase_date, r.description, r.type
 FROM equipment_detail ed
          INNER JOIN room r ON ed.room_id = r.unique_id
          INNER JOIN equipment e ON ed.room_id = e.id
-ORDER BY r.number ASC
-    LIMIT 10 OFFSET 0;
+ORDER BY r.description
+LIMIT 10 OFFSET 0;
 
 
-SELECT r.unique_id, r.number, r.type, r.capacity, r.room_condition
+SELECT r.unique_id, r.description, r.type, r.capacity, r.status
 FROM room r
 WHERE r.type = 'phòng thí nghiệm'
-  AND r.room_condition = 'available'
+  AND r.status = 'available'
   AND NOT EXISTS (
     SELECT 1
     FROM room_schedules rs
